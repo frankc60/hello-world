5778dc5f79ca44025e890c9d6318bf03b66ab572
frankc60 <frankc60@gmail.com>
2021-08-03 20:07:34 +1200

    added jest tests

--- __tests__/maths.test.js
+++ __tests__/maths.test.js
@@ -11,3 +11,25 @@ test('adds 41 + 80 to equal 121', () => {
 test('subtract 9 - 3 to equal 6', () => {
   expect(subtract(9, 3)).toBe(6);
 });
+
+test('object assignment', () => {
+  const data = {one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({one: 1, two: 2});
+});
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           This won't work because of rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+
