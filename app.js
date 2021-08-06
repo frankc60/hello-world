b86d156350e8eac393629f167b830df16e067d18
Frank <frankc60@gmail.com>
2021-08-06 11:56:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   res.send(`Hello World! ${HOST}:${PORT}.
-  the end. changed Friday 6 Aug. now is: ${fullUrl}
+  the end. changed Friday 6 Aug. now isdddd: ${fullUrl}
   ${nowt}`);
 })
 
